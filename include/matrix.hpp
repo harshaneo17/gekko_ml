fc53d4c7fc1d1826994e8b2cbd506d14a4742553
harshaneo17 <harsha.god86@gmail.com>
2023-12-22 23:05:53 +0000

    new classes

---
+++ include/matrix.hpp
@@ -0,0 +1,10 @@
+#ifndef MATRIX_OBJ
+#define MATRIX_OBJ
+
+
+
+
+
+
+
+#endif
---
+++ include/tensor_load.hpp
@@ -0,0 +1,21 @@
+#ifndef TENSOR_LOAD
+#define TENSOR_LOAD
+
+
+class tensor_ops{
+    public:
+      tensor_ops(){};
+      
+      void matrix_add(){
+         return;
+      }
+
+
+};
+
+
+
+
+
+
+#endif
