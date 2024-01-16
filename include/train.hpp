8610b5bf4808afc81798f3acdbdc2338d2434eb6
harshaneo17 <harsha.god86@gmail.com>
2024-01-16 21:13:32 +0000

    new changes to train hpp

--- include/train.hpp
+++ include/train.hpp
@@ -10,9 +10,16 @@ class Train{
     public:
 
         void train(NeuralNet& net,Tensor& inputs,Tensor& targets,int& num_epochs,DataIterator& BatchIterator(),Loss& loss,Optimizer& optimizer){
-            for (epoch : num_epochs){
+            for (auto& epoch : num_epochs){
                 double epoch_loss = 0.0;
-                    for 
+                for (auto& batch : ) { //figure out an alternative for iterator
+                    auto predicted = net.forward(btach.inputs);
+                    auto epoch_loss += loss.loss(predicted, batch.targets);
+                    auto grad = loss.grad(predicted, batch.targets); 
+                    net.backward(grad);
+                    optimizer.step(net); 
+                }
+                std::cout << "epoch number " << epoch << "epoch loss is "   << epoch_loss;
             }
 
         }
