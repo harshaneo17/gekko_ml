185b7ba852e3adbc5ed422dc589fc11c05216dcc
harshaneo17 <harsha.god86@gmail.com>
2024-02-03 13:48:48 +0000

    gui function updated

--- include/train.hpp
+++ include/train.hpp
@@ -14,6 +14,23 @@ class Train{
     public:
         void gui_train(){
             /*this function uses screen width and multiple for loops to write screen*/
+                float progress = 0.0;
+                while (progress < 1.0) {
+                    int barWidth = 70;
+            
+                    std::cout << "[";
+                    int pos = barWidth * progress;
+                    for (int i = 0; i < barWidth; ++i) {
+                        if (i < pos) std::cout << "=";
+                        else if (i == pos) std::cout << ">";
+                        else std::cout << " ";
+                    }
+                    std::cout << "] " << int(progress * 100.0) << " %\r";
+                    std::cout.flush();
+            
+                    progress += 0.16; // for demonstration only
+                }
+                std::cout << std::endl;
         }
 
         
