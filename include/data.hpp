bce9932ca851f4ccccdeaa1643ff8c0165b3dca3
harshaneo17 <harsha.god86@gmail.com>
2024-03-09 10:23:50 +0000

    removed commented function

--- include/data.hpp
+++ include/data.hpp
@@ -19,18 +19,6 @@ class BatchIterator : public DataIterator {
 public:
     
     BatchIterator(int batch_size = 32, bool shuffle = true) : batch_size(batch_size), shuffle(shuffle) {}
-
-    // std::vector<double> arange(double start, double stop, double step) {
-    //     if (step > stop){
-    //         throw std::invalid_argument( "reduce batch size: its greater than input size" );
-    //     }
-    //     std::vector<double> result;
-    //     for (double value = start; value < stop; value += step) {
-    //         result.push_back(value);
-    //         value = std::min(value + step, stop);
-    //     }
-    //     return result;
-    // }
     
     std::vector<Batch> initialize(Tensor inputs, Tensor targets) override {
         std::vector<Batch> batches;
