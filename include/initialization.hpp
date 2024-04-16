63f413005d23e50010e7e3c27785f1b4cc7ba706
harshaneo17 <harsha.god86@gmail.com>
2024-04-16 21:08:02 +0100

    added He class

--- include/initialization.hpp
+++ include/initialization.hpp
@@ -2,12 +2,22 @@
 
 class Glorot{
     public:
-        Tensor intialize(double n_rows,double n_cols){
+        Tensor initialize(double n_rows,double n_cols){
             std::tuple <double, double> size;
             double a = 2 / (n_rows + n_cols);
             Tensor scale = xt::sqrt(a);
             size = make_tuple(n_rows,n_cols);
             return xt::random::randn<double>({scale,size});
         }
+};
+
+class He{
+    Tensor initialize(double n_rows, double n_cols){
+        std::tuple <double, double> size;
+        size = make_tuple(n_rows,n_cols);
+        Tensor low = xt::sqrt(6/n_rows);
+        Tensor high = xt::sqrt(6/n_cols);
+        return xt::random::rand<double>({low,high,size});
+    }
 };
 
